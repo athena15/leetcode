d47e89389d7d3f787e1095bff1370e5275da5f94
Brenner Heintz <athena19@protonmail.com>
2018-12-26 12:42:32 -0500

    Added substring with concatenation of words solution.

---
+++ substring_with_concatenation_of_all_words.py
@@ -0,0 +1,32 @@
+from itertools import permutations
+
+import regex as re
+
+
+class Solution:
+    def findSubstring(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: List[int]
+        """
+        if words:
+            targets = [''.join(i) for i in list(permutations(words, len(words)))]
+        else:
+            return []
+
+        index_matches = []
+        for target in targets:
+            if target in s:
+                regex = r'(' + target + r')'
+                matches = re.finditer(regex, s, overlapped=True)
+                if matches:
+                    indexes = [m.start(0) for m in matches]
+                    print(indexes)
+                    index_matches.extend(indexes)
+
+        return list(set(index_matches))
+
+
+solution = Solution()
+solution.findSubstring(s='foobarfoobar', words=['foo', 'bar'])
