26c6cbd6a02cb8ed1e4fbc7cd5484615cf02c53f
Brenner Heintz <brenner.heintz@gmail.com>
2019-01-19 22:04:06 -0800

    Added solution to search_insert_position.py.

---
+++ search_insert_position.py
@@ -0,0 +1,16 @@
+class Solution:
+	def searchInsert(self, nums, target):
+		"""
+		:type nums: List[int]
+		:type target: int
+		:rtype: int
+		"""
+		for i, v in enumerate(nums):
+			if v >= target:
+				return i
+
+		return len(nums)
+
+
+solution = Solution()
+solution.searchInsert([1, 3, 5, 6], 5)
