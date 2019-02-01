bb571435769b38cfe88aac57fa3445dc3802671c
Brenner Heintz <brenner.heintz@gmail.com>
2019-02-01 15:09:11 -0800

    Added backtracking solution to letter_combinations_of_a_phone_number.py.

--- letter_combinations_of_a_phone_number.py
+++ letter_combinations_of_a_phone_number.py
@@ -3,6 +3,7 @@
 from itertools import product
 
 
+# Concise method using cartesian product
 class Solution:
 	def letterCombinations(self, digits):
 		"""
@@ -22,5 +23,43 @@ class Solution:
 		return [''.join(_) for _ in product(*possibilities)]
 
 
+# Verbose method using backtracking, less concise, but faster runtime
+class Solution_2:
+	def letterCombinations(self, digits):
+		"""
+		:type digits: str
+		:rtype: List[str]
+		"""
+
+		if not digits:
+			return []
+
+		d = {'2': ['a', 'b', 'c'],
+		     '3': ['d', 'e', 'f'],
+		     '4': ['g', 'h', 'i'],
+		     '5': ['j', 'k', 'l'],
+		     '6': ['m', 'n', 'o'],
+		     '7': ['p', 'q', 'r', 's'],
+		     '8': ['t', 'u', 'v'],
+		     '9': ['w', 'x', 'y', 'z']}
+
+		results = []
+
+		def backtrack(remaining_digits, current_string):
+			if len(remaining_digits) == 1:
+				for n in d[remaining_digits]:
+					results.append(current_string + n)
+			else:
+				for n in d[remaining_digits[0]]:
+					backtrack(remaining_digits[1:], current_string + n)
+
+			return results
+
+		return backtrack(digits, '')
+
+
 solution = Solution()
 solution.letterCombinations('239')
+
+solution_2 = Solution_2()
+solution_2.letterCombinations('239')
